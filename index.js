924a451a5e1d71d35ff567c4a8b36d6f4e56a12d
James Lopez <>
2018-04-26 17:53:24 -0700

    Added Mongo connection

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
-const cool = require('cool-ascii-faces')
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express = require('express');
+var mongodb = require('mongodb');
+var router = express.Router();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-    .get('/cool', (req,res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+/* GET home page. */
+router.get('/', function(req, res, next) {
+           res.render('index', { title: 'Express' });
+           });
+
+module.exports = router;
+
+
+//**************************************************************************
+//***** mongodb get all of the Routes in Routes collection where frequence>=1
+//      and sort by the name of the route.  Render information in the views/pages/mongodb.ejs
+router.get('/mongodb', function (request, response) {
+           
+           mongodb.MongoClient.connect('mongodb://heroku_117sqmxb:testpass1@ds159129.mlab.com:59129/heroku_117sqmxb', function(err, client) {
+                                       // mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, db) {  // works with mongodb v2 but not v3
+                                       if(err) throw err;
+                                       //get collection of routes
+                                       var db = client.db('heroku_pmk6n54s');  // in v3 we need to get the db from the client
+                                       var Routes = db.collection('Routes');
+                                       //get all Routes with frequency >=1
+                                       Routes.find({ frequency : { $gte: 0 } }).sort({ name: 1 }).toArray(function (err, docs) {
+                                                                                                          if(err) throw err;
+                                                                                                          
+                                                                                                          response.render('mongodb', {results: docs});
+                                                                                                          
+                                                                                                          });
+                                       
+                                       //close connection when your app is terminating.
+                                       // db.close(function (err) {
+                                       client.close(function (err) {
+                                                    if(err) throw err;
+                                                    });
+                                       });//end of connect
+           });//end app.get
+
+
+
+
